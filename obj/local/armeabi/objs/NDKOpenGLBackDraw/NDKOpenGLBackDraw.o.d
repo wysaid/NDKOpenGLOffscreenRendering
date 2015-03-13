4a408d6a99a178038e3ba6dfba5fe50a52faf260
wangyang <admin@wysaid.org>
2015-03-13 14:23:28 +0800

    init repo

---
+++ obj/local/armeabi/objs/NDKOpenGLBackDraw/NDKOpenGLBackDraw.o.d
@@ -0,0 +1,41 @@
+obj/local/armeabi/objs/NDKOpenGLBackDraw/NDKOpenGLBackDraw.o: \
+ jni/NDKOpenGLBackDraw.cpp \
+ D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/jni.h \
+ D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/android/api-level.h \
+ D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/android/log.h \
+ D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/android/bitmap.h \
+ D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/stdint.h \
+ D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/sys/_types.h \
+ D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/machine/_types.h \
+ D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/GLES2/gl2.h \
+ D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/GLES2/gl2platform.h \
+ D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/KHR/khrplatform.h \
+ D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/GLES2/gl2ext.h
+
+D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/jni.h:
+
+D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/android/api-level.h:
+
+D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/android/log.h:
+
+D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/android/bitmap.h:
+
+D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/stdint.h:
+
+D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/sys/_types.h:
+
+D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/machine/_types.h:
+
+D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/GLES2/gl2.h:
+
+D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/GLES2/gl2platform.h:
+
+D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/KHR/khrplatform.h:
+
+D:/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/GLES2/gl2ext.h:
